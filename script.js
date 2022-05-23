1ff278b0d45fa227fcfc25d90f7388b38d3184be
José Aparecido <juniorsilva240803@gmail.com>
2022-05-23 19:11:14 -0300

    Alterações --> logo, classe names and + 😊😊

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
-$( document ).ready(function() {
-    let menu = document.querySelector('#mobile-menu');
-    let menuLinks = document.querySelector(".navbarr__menu");
-    menu.addEventListener('click', function(){
-        if(menu.classList.toggle('is-active')){
-          $("section").addClass("main-blur");
-          $('.main').click(function() {
-            //Hide the menus if visible
-            console.log("Entrei");
-            $(".navbarr__menu").removeClass("active");
-            // $(".navbarr__menu").hide();
-            $("section").removeClass("main-blur");
-            $("#mobile-menu").removeClass("is-active");
-            
-          }),
-          $('.main').click(function(event){
-            event.stopPropagation();
-            console.log("Sai");
-            
-        });          
-        } else {
-          $("section").removeClass("main-blur");
-        }
-        menuLinks.classList.toggle('active');
+$(document).ready(function () {
+  let menu = document.querySelector("#mobile-menu");
+  let menuLinks = document.querySelector(".navbarr__menu");
+  menu.addEventListener("click", function () {
+    if (menu.classList.toggle("is-active")) {
+      $("section").addClass("main-blur");
+      $(".main").click(function () {
+        //Hide the menus if visible
+        console.log("Entrei");
+        $(".navbarr__menu").removeClass("active");
+        // $(".navbarr__menu").hide();
+        $("section").removeClass("main-blur");
+        $("#mobile-menu").removeClass("is-active");
+      }),
+        $(".main").click(function (event) {
+          event.stopPropagation();
+          console.log("Sai");
+        });
+    } else {
+      $("section").removeClass("main-blur");
+    }
+    menuLinks.classList.toggle("active");
+  });
+
+  // var confettiElement = document.getElementById("my-canvas");
+  // var confettiSettings = { target: confettiElement };
+  // var confetti = new ConfettiGenerator(confettiSettings);
+
+  $("#resumeID").on("click", function (e) {
+    // startConfetti();
+    // confetti.render();
+    Swal.fire({
+      html:
+        '<p style = "color: #64FED9; margin-bottom: 15px;">My <span><b>resume</b></span> <p>' +
+        '<embed src="images/Resume.pdf" type="application/pdf" width="100%" height="600px" />',
+      showCloseButton: true,
+      showCancelButton: false,
+      background: "#01161E",
+      confirmButtonColor: "#112240",
+      cancelButtonColor: "#8892B0",
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+      focusConfirm: true,
+      width: "800px",
     });
-
-    var confettiElement = document.getElementById('my-canvas');
-    var confettiSettings = { target: confettiElement };
-    var confetti = new ConfettiGenerator(confettiSettings);
-
-    $("#resumeID").on("click", function (e) {
-      // startConfetti();
-      // confetti.render();
-      Swal.fire({ 
-           html:
-           '<p style = "color: #64FED9; margin-bottom: 15px;">My <span><b>resume</b></span> <p>' +
-          '<embed src="images/Resume.pdf" type="application/pdf" width="100%" height="600px" />',
-        showCloseButton: true,
-        showCancelButton: false,
-        background: "#01161E",
-        confirmButtonColor: "#112240",
-        cancelButtonColor: "#8892B0",
-        confirmButtonText:
-          '<i class="fa fa-thumbs-up"></i> Great!',
-        focusConfirm: true,
-        width: '800px'
-      })    
-    }),
-
-    finaltext = "José \xa0Aparecido \xa0[Zé].";
-    const characters =
-      "!#$%&'()*+,-./:;<=>?@[]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  }),
+    (finaltext = "José \xa0Aparecido \xa0[Zé].");
+  const characters =
+    "!#$%&'()*+,-./:;<=>?@[]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   iterations = finaltext.length + 20;
   function randomstr() {
@@ -99,20 +95,33 @@ $( document ).ready(function() {
   }
 
   const inst = setInterval(change, 150); //delay da animação
-  }); 
 
-
-  const cursor = $('.cursor');
-  document.addEventListener('mousemove', e => {
-    $('.cursor').attr("style", "top: "+(e.pageY - 15) +"px; left: "+ (e.pageX - 16) +"px;")
-    $('.cursor-inside').attr("style", "top: "+(e.pageY - 7.5) +"px; left: "+ (e.pageX - 8.5) +"px;")
+  const cursor = $(".cursor");
+  document.addEventListener("mousemove", (e) => {
+    $(".cursor").attr(
+      "style",
+      "top: " + (e.pageY - 15) + "px; left: " + (e.pageX - 16) + "px;"
+    );
+    $(".cursor-inside").attr(
+      "style",
+      "top: " + (e.pageY - 7.5) + "px; left: " + (e.pageX - 8.5) + "px;"
+    );
   });
 
-  document.addEventListener('click', () => {
-    $('.cursor').addClass('expand');
+  document.addEventListener("click", () => {
+    $(".cursor").addClass("expand");
 
     setTimeout(() => {
-      $('.cursor').removeClass('expand');
-    }, 500)
-    // $('.cursor-inside').attr("style", "top: "+(e.pageY - 7.5) +"px; left: "+ (e.pageX - 8.5) +"px;")
+      $(".cursor").removeClass("expand");
+    }, 500);
+  });
+
+  $("a").mouseover(() => {
+    $(".cursor").addClass("expand-link");
+    $(".inside-circle").removeClass("cursor-inside");
+  });
+  $("a").mouseout(() => {
+    $(".cursor").removeClass("expand-link");
+    $(".inside-circle").addClass("cursor-inside");
+  });
 });
